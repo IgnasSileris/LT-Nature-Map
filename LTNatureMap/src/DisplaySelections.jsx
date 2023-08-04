3b676301ff63afd9ea41cbfeea2f3744fbc41757
Ignas Sileris <ignasileris@gmail.com>
2023-08-04 12:46:59 +0300

    Initialise

---
+++ LTNatureMap/src/DisplaySelections.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './input.css'
+
+function DisplaySelections() {
+    return (
+        <div>
+            <button> Map </button>
+        </div>
+        
+    );
+}
+
+export default DisplaySelections;
