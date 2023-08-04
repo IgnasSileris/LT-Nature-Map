b0cce1af1a330f4561e4661cd3aec3bd81894291
Ignas Sileris <ignasileris@gmail.com>
2023-08-04 12:47:44 +0300

    Initialise

---
+++ LTNatureMap/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import DisplaySelections from './DisplaySelections'
+import './input.css'
+
+function App() {
+  return (
+    <div>
+      <DisplaySelections/> 
+    </div>
+  );
+}
+
+export default App
