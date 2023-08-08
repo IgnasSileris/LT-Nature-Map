2da1e31dc978f39ece52522c7b0f053abeb7ac01
Ignas Sileris <ignasileris@gmail.com>
2023-08-08 18:34:38 +0300

    Limited zoom and pan to LT only

--- src/DisplaySelection.jsx
+++ src/DisplaySelection.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 function DisplaySelection( {text, onClick} ) {
     return (
         <button className="border-solid border bg-rose-400 p-3">
-            <text>
-                {text}
-            </text>
+            {text}
         </button>
         
     );
--- src/LTMap.jsx
+++ src/LTMap.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import {MapContainer, TileLayer} from 'react-leaflet';
-import "leaflet/dist/leaflet.css"
+import {MapContainer, TileLayer, GeoJSON} from 'react-leaflet';
+import "leaflet/dist/leaflet.css";
+import LTBoundary from "../Extras/LT-Borders.json";
 
 const LTCenter = [55.1694, 23.8813];
-
+const maxBounds = [
+    [ 53.85, 20.5],
+    [ 56.5, 26.9],
+];
 function LTMap() {
     return (
         <div className="absolute top-1/8 left-1/8 w-4/6 h-4/5 border-2 bg-slate-100 ">
-            <MapContainer center={LTCenter} zoom={7} style={{ height: '100%', width: '100%' }}>
+            <MapContainer center={LTCenter} zoomSnap = {0.5} zoom={7.5} minZoom={7.5} maxBounds={maxBounds} style={{ height: '100%', width: '100%' }}>
                 <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
+                <GeoJSON data={LTBoundary} style={{ fillColor: 'green', color: 'green', weight: 2 }} />
             </MapContainer>
         </div>
     );
