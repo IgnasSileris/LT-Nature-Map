05bee5daab4dc258b5ac065a60fcc3c675241d1a
Ignas Sileris <ignasileris@gmail.com>
2023-09-27 20:31:00 +0100

    Progress on signup modal styling

--- src/Components/SignupModal.jsx
+++ src/Components/SignupModal.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { closeSignupModal } from '../reducers/signupModalStatusReducer';
 import { openLoginModal } from '../reducers/loginModalStatusReducer';
@@ -8,6 +8,7 @@ import "primereact/resources/primereact.min.css";
 import { InputText } from 'primereact/inputtext';
 import { Password } from 'primereact/password';
 import '../ExtraCSS/custom.css';
+import { faCircleCheck, faCircleXmark, faCircleMinus } from '@fortawesome/free-solid-svg-icons';
 
 function SignupModal() {
     const signupModalStatus = useSelector((state) => state.signupModalStatus);
@@ -16,6 +17,56 @@ function SignupModal() {
         dispatch(closeSignupModal());
         dispatch(openLoginModal());
     }
+
+    // Input value manager
+    //Email
+    const [emailValue, setEmailValue] = useState('');
+    
+    const handleEmailChange = (e) => {
+        setEmailValue(e.target.value);
+    }
+
+    //Username
+    const [usernameValue, setUsernameValue] = useState('');
+    const [usernameStatus, setUsernameStatus] = useState('Invalid') //options: 'Invalid" - incorrect format, 'Valid' - valid and free, 'Taken' - already in use
+    
+    const handleUsernameChange = (e) => {
+        setUsernameValue(e.target.value);
+        checkUsernameValidity(e.target.value);
+    }
+    function isChar(cCode) {
+        if (('a'.charCodeAt(0) <= cCode && cCode <= 'z'.charCodeAt(0)) || ('A'.charCodeAt(0) <= cCode && cCode <= 'Z'.charCodeAt(0))) {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    const checkUsernameValidity = (uValue) => {
+        const takenUsernames = ["Alice", "Bob", "Charlie", "David", "Eve"];
+        if (uValue.length >= 2 && isChar(uValue.charCodeAt(0))){
+            setUsernameStatus('Valid');
+            if (takenUsernames.includes(uValue)) {
+                setUsernameStatus('Taken');
+            }
+        }
+        else{
+            setUsernameStatus('Invalid');
+        }
+    }
+    let boxClass = '';
+    switch (usernameStatus){
+        case 'Invalid':
+            boxClass = 'test-class-invalid';
+            break;
+        case 'Valid':
+            boxClass = 'test-class-valid';
+            break;
+        case 'Taken':
+            boxClass = 'test-class-taken';
+            break;
+    }
+    //Password
 
     return (
         <ModalDialog isDialogVisible={signupModalStatus} closeDialog={() => dispatch(closeSignupModal())}
@@ -29,19 +80,31 @@ function SignupModal() {
                     <div className="border border-red-800 flex items-center justify-center text-2xl font-bold" style={{width: '100%', height: '15%'}}>
                         Create an account
                     </div>
-                    <div className="border border-red-800 flex items-center" style={{width:'100%', height:'20%'}}>
-                        Email
+                    <div className="border border-red-800 flex items-start" style={{width:'100%', height:'15%'}}>
+                        <span className="p-float-label w-full">
+                            <InputText type="text" className="custom-inputs" maxLength={25} value={emailValue} onChange={handleEmailChange}/>
+                            <label htmlFor="username">Enter your email</label>
+                        </span>
                     </div>
-                    <div className="border border-red-800 flex items-center" style={{width:'100%', height:'15%'}}>
-                        Username
+                    <div className="border border-red-800 flex items-start" style={{width:'100%', height:'15%'}}>
+                        <span className="p-float-label w-full">
+                            <InputText type="text" className={boxClass} maxLength={25} value={usernameValue} onChange={handleUsernameChange}/>
+                            <label htmlFor="username">Choose a username</label>
+                        </span>
                     </div>
-                    <div className="border border-red-800 flex items-center" style={{width:'100%', height:'25%'}}>
-                        Create Password
+                    <div className="border border-red-800 flex items-start" style={{width:'100%', height:'25%'}}>
+                        <span className="p-float-label w-full">
+                            <Password style={{width: '100%'}} inputClassName="custom-inputs" inputId="password" maxLength={25} toggleMask feedback={false}/>
+                            <label htmlFor="password">Create a password</label>
+                        </span>
+                    </div>
+                    <div className="border border-red-800 flex items-start" style={{width:'100%', height:'15%'}}>
+                        <span className="p-float-label w-full">
+                            <Password style={{width: '100%'}} inputClassName="custom-inputs" inputId="password" maxLength={25} toggleMask feedback={false}/>
+                            <label htmlFor="password">Confirm your password</label>
+                        </span>
                     </div>
                     <div className="border border-red-800 flex items-center justify-center" style={{width:'100%', height:'15%'}}>
-                        Confirm Password
-                    </div>
-                    <div className="border border-red-800 flex items-center justify-center" style={{width:'100%', height:'10%'}}>
                         <button className="border-solid border bg-rose-400 hover:bg-rose-500 px-4 py-2 rounded-md">Sign up</button>
                     </div>
                     <div className="border border-red-800 flex items-center justify-center" style={{width:'100%', height:'5%'}}>
